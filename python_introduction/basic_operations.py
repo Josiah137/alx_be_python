d166d9a2d0fbcb0c91ffdab64211662e722007d7
josiah37 <eyosi50@gmail.com>
2024-09-13 19:44:44 +0300

    py into repo

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+# variable defination
+number1 = 10
+number2 = 5
+
+print(f"Addition of {number1} and {number2} is {number1 + number2}\nSubtraction of {number1} and {number2} is {number1 - number2}\nMultiplication of {number1} and {number2} is {number1 * number2}\n")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,9 @@
+# converting  Hours to Seconds
+hours = 2
+
+# calc no of sec in one hour and multiply it with given sec
+seconds = 3600 * hours
+
+print(f"{hours} hour(s) is {seconds} seconds.")
+
+
