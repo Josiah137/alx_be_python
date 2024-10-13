86c37b8fa98e9b0c2197beb8d560e28004caa4f8
josiah37 <eyosi50@gmail.com>
2024-10-13 22:04:04 +0300

    task 0

---
+++ oop/book_class.py
@@ -0,0 +1,18 @@
+class Book:  
+    def __init__(self, title: str, author: str, year: int):  
+        """Initialize a Book instance with title, author, and year."""  
+        self.title = title  
+        self.author = author  
+        self.year = year  
+
+    def __del__(self):  
+        """Destructor that prints a deletion message."""  
+        print(f"Deleting {self.title}")  
+
+    def __str__(self):  
+        """Return informal string representation of the book."""  
+        return f"{self.title} by {self.author}, published in {self.year}"  
+
+    def __repr__(self):  
+        """Return official string representation of the book that can recreate it."""  
+        return f"Book('{self.title}', '{self.author}', {self.year})"
